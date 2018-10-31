7bdd79b4c5f5d8bf4158e59d3f767562e0e8f843
Richard Macias <rjny86@gmail.com>
2018-10-31 17:46:55 -0500

    custom attachments data

--- dad.rb
+++ dad.rb
@@ -12,12 +12,16 @@ class Dad < SlackRubyBot::Bot
   end
 
   command('tell me a joke') do |client, data, match|
-    response = HTTParty.get('https://icanhazdadjoke.com/slack', format: :plain)
-    attachments = JSON.parse(response.body, symbolize_names: true)[:attachments]
+    response = JSON.parse(HTTParty.get('https://icanhazdadjoke.com/slack', format: :plain), symbolize_names: true)
+    joke = response[:attachments][0][:text]
+
     client.web_client.chat_postMessage(
       channel: data.channel,
       as_user: true,
-      attachments: attachments
+      attachments: [
+        "fallback": joke,
+        "pretext": joke
+      ]
     )
   end
 end
